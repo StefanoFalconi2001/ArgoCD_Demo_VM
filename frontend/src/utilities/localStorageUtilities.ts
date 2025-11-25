df92f25883cc42e4a075658785f9c52cb321dcfd
Stefano Falconi Caro <tepano2706@gmail.com>
2025-11-25 16:31:23 -0500

    Fixing localSearchinUtilities

--- frontend/src/utilities/localStorageUtilities.ts
+++ frontend/src/utilities/localStorageUtilities.ts
@@ -3,10 +3,12 @@ import { WeatherData } from "@/services/weatherService";
 const STORAGE_KEY = "savedWeathers";
 
 export function saveWeathersToLocalStorage(data: WeatherData[]) {
+  if (typeof window === "undefined") return; // solo en cliente
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
 export function getWeathersFromLocalStorage(): WeatherData[] {
+  if (typeof window === "undefined") return []; // evita error en build/SSR
   const data = localStorage.getItem(STORAGE_KEY);
   return data ? JSON.parse(data) : [];
 }
